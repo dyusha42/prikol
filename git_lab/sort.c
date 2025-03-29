0b842227f406ab9e570e09d9f877a6fe5c345e8a
dyusha42 <jjakekin@gmail.com>
2025-03-29 19:54:07 +0700

    добавлена фукнция сортировки

---
+++ git_lab/sort.c
@@ -0,0 +1,18 @@
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high], i = low - 1, j, temp;
+        for (j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        quicksort(arr, low, i);
+        quicksort(arr, i + 2, high);
+    }
+}
